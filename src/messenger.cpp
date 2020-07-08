2d35a64f4ba61ee6781cdbdf002d72bafc697df7
Nova <technobaboo@gmail.com>
2020-07-08 03:01:33 -0500

    Created sendSignal method

--- src/messenger.cpp
+++ src/messenger.cpp
@@ -11,6 +11,26 @@ Messenger::Messenger(int readFD, int writeFD, Scenegraph *scenegraph) {
 
 uint Messenger::generateMessageID() { return pendingMessages.size(); }
 
+void Messenger::sendSignal(const char *object, const char *method,
+                           std::vector<uint8_t> &argumentsFlexBuffer) {
+
+  builder.Clear();
+
+  auto object_path = builder.CreateString(object);
+  auto method_name = builder.CreateString(method);
+
+  MessageBuilder messageBuilder(builder);
+  messageBuilder.add_type(3);
+  messageBuilder.add_id(generateMessageID());
+  messageBuilder.add_object(object_path);
+  messageBuilder.add_method(method_name);
+  messageBuilder.add_data(argumentsFlexBuffer);
+  auto message = messageBuilder.Finish();
+
+  builder.Finish(message);
+  sendMessage(builder.GetBufferPointer(), builder.GetSize());
+}
+
 void Messenger::sendMessage(uint8_t *buffer, uint size) {
   write(messageWriteFD, &size, 4);
   write(messageWriteFD, buffer, size);
