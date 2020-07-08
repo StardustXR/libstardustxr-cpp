787c253697ff429ffa332250f45125a0886936ba
Nova <technobaboo@gmail.com>
2020-07-08 02:24:32 -0500

    Made Scenegraph fully virtual

--- include/scenegraph.hpp
+++ include/scenegraph.hpp
@@ -3,9 +3,10 @@
 namespace StardustXR {
 
 class Scenegraph {
-  Scenegraph();
+  Scenegraph(){};
+  virtual ~Scenegraph(){};
 
-  pure virtual void executeMethod(std::string path, std::string method);
-}
+  virtual void executeMethod(std::string path, std::string method) = 0;
+};
 
 } // namespace StardustXR
