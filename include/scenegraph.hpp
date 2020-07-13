66c4736c4bb2f4a35ca87f349e77e2f95615a93e
Nova <technobaboo@gmail.com>
2020-07-13 11:02:19 -0500

    Add scenegraph send signal method

--- include/scenegraph.hpp
+++ include/scenegraph.hpp
@@ -11,8 +11,11 @@ public:
   Scenegraph(){};
   virtual ~Scenegraph(){};
 
-  virtual void executeMethod(std::string path, std::string method,
-                             flexbuffers::Reference args) = 0;
+  virtual void sendSignal(std::string path, std::string method,
+                          flexbuffers::Reference data) = 0;
+  virtual std::vector<uint8_t> executeMethod(std::string path,
+                                             std::string method,
+                                             flexbuffers::Reference args) = 0;
 };
 
 } // namespace StardustXR
