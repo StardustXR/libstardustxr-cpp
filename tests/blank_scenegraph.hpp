18143ae71310f03c512d100ad4a7c3c081513c8a
Nova <technobaboo@gmail.com>
2020-07-13 10:57:36 -0500

    Added scenegraph send method

--- tests/blank_scenegraph.hpp
+++ tests/blank_scenegraph.hpp
@@ -7,10 +7,18 @@ class BlankScenegraph : public Scenegraph {
 public:
   explicit BlankScenegraph() : Scenegraph() {}
 
-  void executeMethod(std::string path, std::string method,
-                     flexbuffers::Reference args) {
-    printf("Attempting to execute method %s on object with path %s",
+  void sendSignal(std::string path, std::string method,
+                  flexbuffers::Reference data) {
+    printf("Sending signal with method %s on object with path %s\n",
            method.c_str(), path.c_str());
+    printf("\t");
+  }
+
+  std::vector<uint8_t> executeMethod(std::string path, std::string method,
+                                     flexbuffers::Reference args) {
+    printf("Attempting to execute method %s on object with path %s\n",
+           method.c_str(), path.c_str());
+    return std::vector<uint8_t>();
   }
 };
 
