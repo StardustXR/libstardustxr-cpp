56200112d0d803a6e05a6d3c0322e77f1a4fd2fe
Nova <technobaboo@gmail.com>
2020-09-13 16:25:02 -0500

    Separated client and server messengers for a final time

--- include/messenger.hpp
+++ include/messenger.hpp
@@ -17,14 +17,6 @@ class Messenger {
 public:
 	explicit Messenger(int readFD, int writeFD, Scenegraph *scenegraph);
 
-	void sendSignal(const char *object, const char *method, std::vector<uint8_t> &data);
-	void sendCall(uint8_t type, uint id, const char *object, const char *method, std::vector<uint8_t> &data);
-
-	flexbuffers::Reference executeRemoteMethod(const char *object, const char *method, std::vector<uint8_t> &data);
-	void executeRemoteMethod(const char *object, const char *method, std::vector<uint8_t> &data, void (*asyncCallback)(flexbuffers::Reference));
-
-	void sendMessage(uint8_t *buffer, uint size);
-
 protected:
 	int messageReadFD;
 	int messageWriteFD;
--- include/server/messengermanager.hpp
+++ include/server/messengermanager.hpp
@@ -1,7 +1,7 @@
 #ifndef STARDUSTXR_MESSENGER_MANAGER_H
 #define STARDUSTXR_MESSENGER_MANAGER_H
 
-#include "../messenger.hpp"
+#include "server_messenger.hpp"
 #include "../scenegraph.hpp"
 
 #include <fcntl.h>
