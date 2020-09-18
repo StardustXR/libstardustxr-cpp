ca8f8ec060ed787409b8c336ff279551faaf67d3
Nova <technobaboo@gmail.com>
2020-09-18 05:34:30 -0500

    Pass through ID from method call to method return

--- src/client/messenger.cpp
+++ src/client/messenger.cpp
@@ -38,7 +38,7 @@ void ClientMessenger::handleMessage(const Message *message) {
 	case 2: {
 		// Method was called, so execute the local scenegraph method and send back the result
 		std::vector<uint8_t> returnValue = scenegraph->executeMethod(message->object()->str(), message->method()->str(), message->data_flexbuffer_root());
-		sendCall(3, 0, message->object()->c_str(), message->method()->c_str(), returnValue);
+		sendCall(3, message->id(), message->object()->c_str(), message->method()->c_str(), returnValue);
 	} break;
 	case 3: {
 		//Method return, so execute the callback method if it exists and remove it from the pending map
@@ -47,7 +47,7 @@ void ClientMessenger::handleMessage(const Message *message) {
 			pendingCallbacks.erase(message->id());
 		} else {
 			// I have no idea how this would happen but I don't want anything breaking on the slim chance it does...
-			printf("The method callback \"%s\" on object with path \"%s\" is not pending", message->method()->c_str(), message->object()->c_str());
+			printf("The method callback \"%s\" on object with path \"%s\" and id %i is not pending\n", message->method()->c_str(), message->object()->c_str(), message->id());
 		}
 	}
 	}
