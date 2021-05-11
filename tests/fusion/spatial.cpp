e64c6fcc9468a4dcddd39fed55906686bf37757e
Nova <technobaboo@gmail.com>
2021-05-10 22:13:09 -0500

    Added gyro root and gem

--- tests/fusion/spatial.cpp
+++ tests/fusion/spatial.cpp
@@ -18,17 +18,19 @@ int main(int, char *[]) {
 	Model outsideRing("../../../res/gyro_outside.glb");
 	Model middleRing("../../../res/gyro_middle.glb");
 	Model insideRing("../../../res/gyro_inside.glb");
+	Model gem("../../../res/gyro_gem.glb");
 
 	outsideRing.setSpatialParent(&gyroRoot);
 	middleRing.setSpatialParent(&outsideRing);
 	insideRing.setSpatialParent(&middleRing);
+	gem.setSpatialParent(&gyroRoot);
 
 	LifeCycle()->onLogicStep([&](double delta, double) {
 		time += delta;
 		double rotation = 360*time/rotationSeconds;
 
 		gyroRoot.setOrigin(vec3_up * std::sin(rotation*deg2rad) * 0.1f);
-		gyroRoot.setScale(vec3_one * (1.0f + std::sin(rotation*deg2rad) * 0.25f));
+		gyroRoot.setScale(vec3_one * (1.0f + std::sin(rotation*deg2rad*2) * 0.25f));
 
 		outsideRing.setOrientation(quat_from_angles(0, 0, rotation));
 		middleRing.setOrientation(quat_from_angles(rotation, 0, 0));
