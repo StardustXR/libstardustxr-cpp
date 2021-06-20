100dbc836cfed6ccd390d1bd99ea78ffe2a1d914
technobaboo <technobaboo@gmail.com>
2021-06-20 16:23:27 -0500

    Known working lighting setup

--- tests/fusion/environment.cpp
+++ tests/fusion/environment.cpp
@@ -10,6 +10,8 @@ int main(int, char *[]) {
 	StardustXRFusion::Setup();
 	Environment()->visible(true);
 	Environment()->setSkytex("../../../res/zhengyang_gate_4k.hdr");
-	Environment()->setLighting("../../../res/wide_street_01_1k.hdr");
-	std::this_thread::sleep_for(std::chrono::seconds(3600));
+	// Environment()->setSkytex("../../../res/wide_street_01_1k.hdr");
+	Environment()->setLighting("../../../res/zhengyang_gate_4k.hdr");
+	// Environment()->setLighting("../../../res/wide_street_01_1k.hdr");
+	return 0;
 }
