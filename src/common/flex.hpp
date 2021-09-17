107146cc52eb7a56b9a1cfdb0190aff36426f9f8
Nova <technobaboo@gmail.com>
2021-09-17 04:32:50 -0500

    Add FLEX_NULL

--- src/common/flex.hpp
+++ src/common/flex.hpp
@@ -38,6 +38,8 @@ namespace StardustXR {
 		});
 	#define FLEX_STRING(str) \
 		fbb.String(str.c_str());
+	#define FLEX_NULL \
+		fbb.Null();
 	#define FLEX_BOOL(bol) \
 		fbb.Bool(bol);
 	#define FLEX_FLOAT(flt) \
