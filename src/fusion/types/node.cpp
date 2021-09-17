515291aabbccd42a9c3cfc2de31278e94b005026
Nova <technobaboo@gmail.com>
2021-09-17 04:34:10 -0500

    Call destroy method on local node destruction

--- src/fusion/types/node.cpp
+++ src/fusion/types/node.cpp
@@ -1,9 +1,16 @@
 #include "node.hpp"
+#include "../fusion_internal.hpp"
 
 namespace StardustXRFusion {
 
 Node::Node() {}
-Node::~Node() {}
+Node::~Node() {
+	messenger->sendSignal(
+		getNodePath().c_str(),
+		"destroy",
+		FLEX_ARG(FLEX_NULL)
+	);
+}
 
 std::string Node::getNodePath() {
 	return nodePath+"/"+nodeName;
