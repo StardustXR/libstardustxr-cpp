590f465c4c3d45d61cc28029aa1c16b9579d61da
Nova <technobaboo@gmail.com>
2021-10-22 13:38:43 -0500

    Enhance Fusion scenegraph sendSignal and executeMethod

--- src/fusion/scenegraph.cpp
+++ src/fusion/scenegraph.cpp
@@ -1,17 +1,27 @@
 #include "scenegraph.hpp"
+#include <cstdint>
 
 namespace StardustXRFusion {
 
 FusionScenegraph::FusionScenegraph() {}
 
 void FusionScenegraph::sendSignal(std::string, std::string methodName, flexbuffers::Reference data) {
-	Method *method = &methods[methodName];
-	(*method)(data, false);
+	if(methods.find(methodName) != methods.end()) {
+		Method *method = &methods[methodName];
+		(void)(*method)(data, false);
+	} else {
+		fprintf(stderr, "Client method \"%s\" does not exist", methodName.c_str());
+	}
 }
 
 std::vector<uint8_t> FusionScenegraph::executeMethod(std::string, std::string methodName, flexbuffers::Reference data) {
-	Method *method = &methods[methodName];
-	return (*method)(data, true);
+	if(methods.find(methodName) != methods.end()) {
+		Method *method = &methods[methodName];
+		return (*method)(data, false);
+	} else {
+		fprintf(stderr, "Client method \"%s\" does not exist", methodName.c_str());
+		return std::vector<uint8_t>();
+	}
 }
 
 } // namespace StardustXRFusion
