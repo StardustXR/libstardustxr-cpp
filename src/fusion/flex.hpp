892b384299f89c84474de975111122975e8c26d8
Nova <technobaboo@gmail.com>
2021-10-22 13:36:08 -0500

    Clean up Fusion flex.hpp

--- src/fusion/flex.hpp
+++ src/fusion/flex.hpp
@@ -3,26 +3,24 @@
 
 #pragma once
 
-using namespace SKMath;
-
 namespace StardustXRFusion {
 
 	///////////////////////////////////////////
 	// Conversion from flex to SKMath
 
-	#define SK_VEC2(vec) {\
+	#define SK_VEC2(vec) SKMath::vec2{\
 		vec[0].AsFloat(),\
 		vec[1].AsFloat()\
 	}
-	#define SK_VEC3(vec) {\
+	#define SK_VEC3(vec) SKMath::vec3{\
 		vec[0].AsFloat(),\
 		vec[1].AsFloat(),\
 		vec[2].AsFloat()\
 	}
-	#define SK_QUAT(quat) {\
-		quat[0].AsFloat(),\
-		quat[1].AsFloat(),\
-		quat[2].AsFloat(),\
-		quat[3].AsFloat()\
+	#define SK_QUAT(quat_) SKMath::quat{\
+		quat_[0].AsFloat(),\
+		quat_[1].AsFloat(),\
+		quat_[2].AsFloat(),\
+		quat_[3].AsFloat()\
 	}
 }
