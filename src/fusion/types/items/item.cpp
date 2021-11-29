4540ff6983fc5e4c22354ce26bc7b9735d81e102
Nova <technobaboo@gmail.com>
2021-11-28 18:21:55 -0600

    Make Item not destroyable

--- src/fusion/types/items/item.cpp
+++ src/fusion/types/items/item.cpp
@@ -3,7 +3,11 @@
 
 namespace StardustXRFusion {
 
-Item::Item(Spatial *space, std::string nodePath, std::string nodeName) : Spatial(space, nodePath, nodeName) {}
-Item::Item(Spatial *space, SKMath::vec3 origin, SKMath::quat orientation) : Spatial(space, origin, orientation) {}
+Item::Item(Spatial *space, std::string nodePath, std::string nodeName) : Spatial(space, nodePath, nodeName) {
+	destroyable = false;
+}
+Item::Item(Spatial *space, SKMath::vec3 origin, SKMath::quat orientation) : Spatial(space, origin, orientation) {
+	destroyable = false;
+}
 
 }
