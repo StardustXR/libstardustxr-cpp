d42568157cc706e0dab376b6df1105f0c9372bf1
Nova <technobaboo@gmail.com>
2022-04-04 09:49:37 -0400

    Add single actor action into Fusion

---
+++ src/fusion/types/input/actions/singleactor.cpp
@@ -0,0 +1,41 @@
+#include "singleactor.hpp"
+
+namespace StardustXRFusion {
+
+void SingleActorAction::update() {
+	InputActionHandler::Action::update();
+
+	if(startedActing.size() > 0) {
+		auto newActor = startedActing.end()-1;
+		if(newActor->uuid != actorUUID) {
+			if(!actorActing)
+				actorStarted = true;
+			else
+				actorChanged = true;
+			actorActing = true;
+			actor = newActor.base();
+			actorUUID = actor->uuid;
+			return;
+		}
+	}
+
+	if(!actorActing && actor)
+		actor = nullptr;
+	auto stoppedActor = std::find(stoppedActing.begin(), stoppedActing.end(), actorUUID);
+	if(actorActing && stoppedActor != stoppedActing.end()) {
+		actor = stoppedActor.base();
+		actorUUID = "";
+		actorActing = false;
+		actorStopped = true;
+		return;
+	}
+
+	actorChanged = false;
+	actorStarted = false;
+	actorStopped = false;
+
+	if(actorActing)
+		actor = std::find(activelyActing.begin(), activelyActing.end(), actorUUID).base();
+}
+
+}
---
+++ src/fusion/types/input/actions/singleactor.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "..//types/handinput.hpp"
+#include "..//types/pointerinput.hpp"
+#include "..//inputactionhandler.hpp"
+
+namespace StardustXRFusion {
+
+struct SingleActorAction : public InputActionHandler::Action {
+	bool actorStarted = false;
+	bool actorChanged = false;
+	bool actorActing  = false;
+	bool actorStopped = false;
+
+	StardustXRFusion::InputActionHandler::InputMethod *actor = nullptr;
+
+protected:
+	void update();
+	std::string actorUUID;
+};
+
+}
